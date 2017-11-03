a86c996430ab8334c65ae2b50b162dcb81a5f25d
Ganesh Ram <ganeshram997@gmail.com>
2017-11-04 03:45:44 +0530

    splitting js functions

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+	
+	$("#1").click(function(){
+		$('#selfie-modal').modal('show');
+	});
+		
+	$("#2").click(function(){
+		$('#smart-modal').modal('show');
+	});
+		
+	$("#3").click(function(){
+		$('#e-yantra-modal').modal('show');
+	});
+		
+	$("#4").click(function(){
+		$('#robometry-modal').modal('show');
+	});
+
+});
